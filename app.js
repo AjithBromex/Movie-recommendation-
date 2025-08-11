9a0896219bc2d1714ee901256f2c908f416ae934
AjithBromex <161185036+AjithBromex@users.noreply.github.com>
2025-08-11 12:31:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+
 // Example movie data
 const movies = {
     action: [
-        { title: "Mad Max: Fury Road", poster: "https://m.media-amazon.com/images/I/81w+K6h8nPL._AC_SY679_.jpg" },
-        { title: "John Wick", poster: "https://m.media-amazon.com/images/I/71E8UOqUv7L._AC_SY679_.jpg" },
-        { title: "Die Hard", poster: "https://m.media-amazon.com/images/I/91jR7O5vN1L._AC_SY679_.jpg" }
+        { title: "Mad Max: Fury Road", poster: "https://upload.wikimedia.org/wikipedia/en/6/6e/Mad_Max_Fury_Road.jpg" },
+        { title: "John Wick", poster: "https://m.media-amazon.com/images/I/71+k2-r7vQL._UF1000,1000_QL80_.jpg" },
+        { title: "Die Hard", poster: "https://resizing.flixster.com/4uMRStWZiVgShCGo_ywlOGfK9jA=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzdjMzI5NWU3LTEyNTUtNDRlNC04ZWYxLThlMzZiNDE3YTMxOC5wbmc=" }
     ],
     comedy: [
-        { title: "The Hangover", poster: "https://m.media-amazon.com/images/I/81lK0Bv4L-L._AC_SY679_.jpg" },
-        { title: "Superbad", poster: "https://m.media-amazon.com/images/I/81YJr9bXQzL._AC_SY679_.jpg" },
-        { title: "Step Brothers", poster: "https://m.media-amazon.com/images/I/91-r9gA5RML._AC_SY679_.jpg" }
+        { title: "The Hangover", poster: "https://m.media-amazon.com/images/I/91Z8lOMpS+L._UF1000,1000_QL80_.jpg" },
+        { title: "Superbad", poster: "https://m.media-amazon.com/images/I/51HShnByGEL._UF1000,1000_QL80_.jpg" },
+        { title: "Step Brothers", poster: "https://upload.wikimedia.org/wikipedia/en/d/d9/StepbrothersMP08.jpg" }
     ],
     "sci-fi": [
         { title: "Inception", poster: "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SY679_.jpg" },
@@ -17,8 +18,8 @@ const movies = {
     ],
     drama: [
         { title: "The Shawshank Redemption", poster: "https://m.media-amazon.com/images/I/51NiGlapXlL.jpg" },
-        { title: "Forrest Gump", poster: "https://m.media-amazon.com/images/I/61N4J5Zrq+L._AC_SY679_.jpg" },
-        { title: "Fight Club", poster: "https://m.media-amazon.com/images/I/81D+KJkO5eL._AC_SY679_.jpg" }
+        { title: "Forrest Gump", poster: "https://m.media-amazon.com/images/I/713xd8jGVuL._UF1000,1000_QL80_.jpg" },
+        { title: "Fight Club", poster: "https://m.media-amazon.com/images/M/MV5BOTgyOGQ1NDItNGU3Ny00MjU3LTg2YWEtNmEyYjBiMjI1Y2M5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpgg" }
     ]
 };
 
@@ -43,3 +44,4 @@ function showRecommendations(genre) {
 
 // Show default recommendations
 showRecommendations('action');
+
